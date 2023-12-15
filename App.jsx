1aa19b1b0dffb73c4f002c21647d03e437f76356
nunesfb <nunesfb@gmail.com>
2023-12-15 13:12:28 -0300

    rotas no react native

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import {View, Text} from "react-native";
+import styles from './AppStyles';
+
+export default function App() {
+ 
+  return (
+    <View style={styles.container}>
+      <Text>Rotas e Navegação</Text>
+    </View>
+  );
+}
